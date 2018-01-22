1b0e6f745a7933c10e9fe573aaf000fd9770a587
hamizann <33884116+hamizann@users.noreply.github.com>
2018-01-22 10:29:31 +0800

    Task complete

--- Array-Exercise-3.cpp
+++ Array-Exercise-3.cpp
@@ -1,21 +1,30 @@
 //dapatkan 10 input dan keluarkan hasil tambahnya.
 //hasilkan program anda dengan menggunakan array
 
+#include <iostream>
+#include <conio.h>
+using namespace std;
 
 int array[10];
 int sum;
 int x;
 
+int main()
+{
+
 cout<<"Enter the 10 numbers:"<<endl;
 for(x=0;x<10;x++)
 {
+	cin>>array[x];
+
+sum=sum+array[x];
 
 }
 
-cin>>array[x];
 
-sum=sum+array[x];
 
 cout<<"the sum of the 10 numbers : "<<sum<<endl;
 
 std::cin.get();
+
+}
