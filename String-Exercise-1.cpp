c058c08e5e84a1008189850103a67fd32b0a560e
hamizann <33884116+hamizann@users.noreply.github.com>
2018-01-23 02:06:31 +0800

    Task complete

--- String-Exercise-1.cpp
+++ String-Exercise-1.cpp
@@ -1,13 +1,15 @@
-//include your name and matric number here
+//Ahmad Hamizan Bin Hilmi A17DW0699
 //display the string below using the string function state below.
 
 #include <iostream>
+#include <string.h>
+using namespace std;
 
 int main()
 {
 	char engkau[10];
 
-	//stringcopy(a,"hello");
+	strcpy(engkau,"hello");
 	
 	cout<<engkau;
 
